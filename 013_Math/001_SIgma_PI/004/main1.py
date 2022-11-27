c9e046f4b1177b898ddd20943045da72af3d4d5a
mznmk <mika_develop@mznmk.com>
2022-11-27 15:47:14 +0900

    [Math] solve "for 文とシグマ計算 (4)"

---
+++ 013_Math/001_SIgma_PI/004/main1.py
@@ -0,0 +1,53 @@
+#
+# for 文とシグマ計算 (4)
+#
+# https://algo-method.com/tasks/569
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+
+    # [ simulate ]
+    s = 0
+    for i in range(N):
+        for j in range(i+1, N+1):
+            s += i*j
+
+    # [ output ]
+    print(s)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
