60e33cd58aff8a1dd07a202f9d460461220d1d4c
mznmk <mika_develop@mznmk.com>
2022-11-27 19:05:15 +0900

    [math] solve "貯金計画"

---
+++ 013_Math/003_APGP/002/main1.py
@@ -0,0 +1,51 @@
+#
+# 貯金計画
+#
+# https://algo-method.com/tasks/650
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    A, B, X, Y = map(int, input().split())
+
+    # [ simulate ]
+    d = (Y-X)//(B-A)
+    m = X - d*A
+
+    # [ output ]
+    print(m)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
