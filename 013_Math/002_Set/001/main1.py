d208a1c6d773b0851e8cfbbbe292220dffc28b2a
mznmk <mika_develop@mznmk.com>
2022-11-27 16:36:16 +0900

    [Math] solve "共通する数 (A ∩ B)"

---
+++ 013_Math/002_Set/001/main1.py
@@ -0,0 +1,54 @@
+#
+# 共通する数 (A ∩ B)
+#
+# https://algo-method.com/tasks/586
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N, M = map(int, input().split())
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
+
+    # [ simulate ]
+    C = list(A & B)
+    C.sort()
+
+    # [ output ]
+    for c in C:
+        print(c)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
