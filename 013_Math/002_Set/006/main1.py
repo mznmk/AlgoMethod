b0975e0498315ed6ae21a4fcbbf44fc7e7c3c146
mznmk <mika_develop@mznmk.com>
2022-11-27 17:43:35 +0900

    [math] solve "合併後の個数"

---
+++ 013_Math/002_Set/006/main1.py
@@ -0,0 +1,47 @@
+#
+# 合併後の個数
+#
+# https://algo-method.com/tasks/639
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N, M, K = map(int, input().split())
+
+    # [ output ]
+    print(N+M-K)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
