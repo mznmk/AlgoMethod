58014435d4445a2597ad9e4f3afb614cca734711
mznmk <mika_develop@mznmk.com>
2022-11-27 19:14:48 +0900

    [math] solve "等差数列の和 (1)"

---
+++ 013_Math/003_APGP/003/main1.py
@@ -0,0 +1,50 @@
+#
+# 等差数列の和 (1)
+#
+# https://algo-method.com/tasks/652
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+
+    # [ simulate ]
+    s = (1+N)*N//2
+
+    # [ output ]
+    print(s)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
