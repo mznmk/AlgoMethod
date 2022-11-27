0de41f628f2f78d3a1158361f5efeef711411af5
mznmk <mika_develop@mznmk.com>
2022-11-27 18:37:02 +0900

    [math] solve "書かれた数の個数 (4)"

---
+++ 013_Math/002_Set/008/main1.py
@@ -0,0 +1,53 @@
+#
+# 書かれた数の個数 (4)
+#
+# https://algo-method.com/tasks/641
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+
+    # [ simulate ]
+    A3  = N//3
+    A5  = N//5
+    A15 = N//15
+    A = N - (A3+A5-A15)
+
+    # [ output ]
+    print(A)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
