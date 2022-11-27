5f23ebc54a9ba6897a4b849f012517f0bd028206
mznmk <mika_develop@mznmk.com>
2022-11-27 18:01:45 +0900

    [math] solve "書かれた数の個数 (3)"

---
+++ 013_Math/002_Set/007/main1.py
@@ -0,0 +1,52 @@
+#
+# 書かれた数の個数 (3)
+#
+# https://algo-method.com/tasks/640
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N, X, Y = map(int, input().split())
+    A = set(map(int, input().split()))
+    B = set(map(int, input().split()))
+
+    # [ simulate ]
+    C = N - len(A | B)
+
+    # [ output ]
+    print(C)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
