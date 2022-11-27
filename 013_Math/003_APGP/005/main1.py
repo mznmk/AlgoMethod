9c0d8bcfbc86a3b0bf7af9ba862eacebfa72b279
mznmk <mika_develop@mznmk.com>
2022-11-27 20:15:49 +0900

    [math] solve "2 乗和"

---
+++ 013_Math/003_APGP/005/main1.py
@@ -0,0 +1,50 @@
+#
+# 2 乗和
+#
+# https://algo-method.com/tasks/654
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+
+    # [ simulate ]
+    s = N*(N+1)*(N*2+1)//6
+
+    # [ output ]
+    print(s)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
