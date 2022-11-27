5b509250ba3ac8ec04f203f9f19f32bf5f6e7717
mznmk <mika_develop@mznmk.com>
2022-11-27 21:19:29 +0900

    [math] solve "栗まんじゅう問題"

---
+++ 013_Math/003_APGP/006/main1.py
@@ -0,0 +1,49 @@
+#
+# 栗まんじゅう問題
+#
+# https://algo-method.com/tasks/651
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = 1
+
+    # [ simulate ]
+    for _ in range(0, 20+1):
+        print(N)
+        N *= 2
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
