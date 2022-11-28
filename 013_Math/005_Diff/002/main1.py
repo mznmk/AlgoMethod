9e96301b40f2a51cdc53722eb8c1ae28aac3d424
mznmk <mika_develop@mznmk.com>
2022-11-28 14:37:02 +0900

    [math] solve "直線の傾き (2)"

---
+++ 013_Math/005_Diff/002/main1.py
@@ -0,0 +1,58 @@
+#
+# 直線の傾き (2)
+#
+# https://algo-method.com/tasks/581
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N, A, B = map(int, input().split())
+
+    # [ simulate ]
+    # < function >
+    def f(x):
+        return x*x
+    def findSlope(a, b):
+        return (f(b)-f(a))/(b-a)
+    # < simulate >
+    for _ in range(N):
+        # find slope
+        s = findSlope(A, B)
+        print(f"{s:.10f}")
+        # move next
+        B = (A+B)/2
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
