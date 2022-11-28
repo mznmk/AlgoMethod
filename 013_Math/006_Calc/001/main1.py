b9b1b83545f41f748737eb1f2b626375d6bf1d1d
mznmk <mika_develop@mznmk.com>
2022-11-28 17:46:15 +0900

    [math] solve "面積を求める (1)"

---
+++ 013_Math/006_Calc/001/main1.py
@@ -0,0 +1,52 @@
+#
+# 面積を求める (1)
+#
+# https://algo-method.com/tasks/602
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+
+    # [ simulate ]
+    S = 0
+    for s in range(1, N+1):
+        S += s
+
+    # [ output ]
+    print(S)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
