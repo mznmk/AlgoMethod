ad5e2e50364302da94b132cd622116e17afb069f
mznmk <mika_develop@mznmk.com>
2022-12-18 07:32:10 +0900

    [BaseN] solve "N 進法を 10 進法にするプログラム"

---
+++ 035_BaseN/002_BaseN/001/main1.py
@@ -0,0 +1,55 @@
+#
+# N 進法を 10 進法にするプログラム
+#
+# https://algo-method.com/tasks/1185
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+    S = list(input())
+    M = len(S)
+
+    # [ simulate ]
+    S.reverse()
+    V = 0
+    for i in range(M):
+        V += N**i * int(S[i])
+
+    # [ output ]
+    print(V)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
---
+++ 035_BaseN/002_BaseN/001/main2.py
@@ -0,0 +1,56 @@
+#
+# N 進法を 10 進法にするプログラム
+#
+# https://algo-method.com/tasks/1185
+
+# ---------------------------------------------------------------------------- #
+
+from time import process_time
+import sys
+sys.setrecursionlimit(10**9)
+
+MOD9 = 10**9+9
+MOD7 = 10**9+7
+MOD3 = 998244353
+
+# ---------------------------------------------------------------------------- #
+
+def solve():
+    # [ input ]
+    N = int(input())
+    S = list(input())
+
+    # [ simulate ]
+    S.reverse()
+    P = 1
+    V = 0
+    for s in S:
+        V += P * (ord(s)-ord("0"))
+        P *= N
+
+    # [ output ]
+    print(V)
+
+    # [ return ]
+    return
+
+# ---------------------------------------------------------------------------- #
+
+if __name__ == '__main__':
+    if __debug__:
+        # [ submit ]
+        solve()
+    else:
+        # [ test samples ]
+        # set I/O
+        sys.stdin = open('input.txt', 'r')
+        sys.stdout = open('output.txt', 'w')
+        # test samples
+        times = int(input())
+        for i in range(1, times+1):
+            input()
+            print(f'[ sample: {i} ]')
+            start = process_time()
+            solve()
+            end = process_time()
+            print(f'(Run Time: {(end-start)*1000:f} ms)\n')
